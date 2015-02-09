2eef405b314f47f2f7c73b30b4e4d642a4c1f2d4
Veronica Baldys <veronicabaldys@gmail.com>
2015-02-08 20:07:29 -0800

    initial commit

---
+++ WeatherViewController.m
@@ -0,0 +1,13 @@
+//
+//  WeatherViewController.m
+//  Active Alarm
+//
+//  Created by Veronica Baldys on 2015-02-08.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import "WeatherViewController.h"
+
+@implementation WeatherViewController
+
+@end
