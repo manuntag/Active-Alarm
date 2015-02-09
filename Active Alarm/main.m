155afdb83f3db2cd02e0ee1eac1a10c5fc224e85
manuntag <manuntag@ualberta.ca>
2015-02-08 19:47:53 -0800

    Initial Commit

---
+++ Active Alarm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Active Alarm
+//
+//  Created by David Manuntag on 2015-02-08.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Active AlarmTests/Active_AlarmTests.m
@@ -0,0 +1,40 @@
+//
+//  Active_AlarmTests.m
+//  Active AlarmTests
+//
+//  Created by David Manuntag on 2015-02-08.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Active_AlarmTests : XCTestCase
+
+@end
+
+@implementation Active_AlarmTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
